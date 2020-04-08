6df99810e520173f24a7ad983b757acdd8ee9ecc
HIRONORI KIKUCHI <hironori.kikuchi.pgm@gmail.com>
2020-04-08 07:59:49 +0700

    add: sample component with and without decorator
    ./src/components/WithoutDecorator.vue
    ./src/components/WithDecorator.vue

---
+++ src/components/WithDecorator.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <input v-model="msg" />
+    <p>prop: {{ propMessage }}</p>
+    <p>msg: {{ msg }}</p>
+    <p>helloMsg: {{ helloMsg }}</p>
+    <p>computedMsg: {{ computedMsg }}</p>
+    <button @click="greet">Greet</button>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import Component from "vue-class-component";
+
+@Component
+export default class WithDecorator extends Vue {
+  msg = "Hiro";
+  helloMsg = "Hello, " + this.msg;
+
+  mounted() {
+    this.greet();
+  }
+
+  get computedMsg() {
+    return "computed " + this.msg;
+  }
+
+  greet() {
+    alert("greeting " + this.msg);
+  }
+}
+</script>
---
+++ src/components/WithoutDecorator.vue
@@ -0,0 +1,44 @@
+<template>
+  <div>
+    <div class="greeting">Hello, {{ name }} {{ exclamationMarks }}</div>
+    <button @click="decrement">-</button>
+    <button @click="increment">+</button>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+export default Vue.extend({
+  props: {
+    name: {
+      type: String,
+      default: ""
+    },
+    initialEnthusiasm: {
+      type: Number,
+      default: 0
+    }
+  },
+  data() {
+    return {
+      enthusiasm: this.initialEnthusiasm
+    };
+  },
+  methods: {
+    increment() {
+      this.enthusiasm++;
+    },
+    decrement() {
+      if (this.enthusiasm > 1) {
+        this.enthusiasm--;
+      }
+    }
+  },
+  computed: {
+    exclamationMarks(): string {
+      return Array(this.enthusiasm + 1).join("!");
+    }
+  }
+});
+</script>
